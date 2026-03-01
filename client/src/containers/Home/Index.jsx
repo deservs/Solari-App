0377f44cf6fd71c4e3c4d1299f94afe82aebddad
deservs <carlos.rodrigues151@etec.sp.gov.br>
2026-03-01 19:20:12 -0300

    Adições do home: - Adicionado um Carrossel utilizando a biblioteca Swiper para exibir os filmes - Teste de integração dos filmes utilizando a API do TMDB

--- client/src/containers/Home/Index.jsx
+++ client/src/containers/Home/Index.jsx
@@ -1,32 +1,64 @@
 import { useEffect, useState } from "react";
 
+import { Navigation, Virtual } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 import { getTrendingMovies } from "../../services/MovieList";
 
 import Header from "../../components/Header/Header";
+import "swiper/css";
+import "swiper/css/navigation";
 
 import "./style.css";
+
 function Home() {
+  return (
+    <>
+      <Header />
+      <MyTrending />
+    </>
+  );
+}
+
+const MyTrending = () => {
   const [filmes, setFilmes] = useState([]);
+
   useEffect(() => {
     const carregarDados = async () => {
       const listaDeFilmes = await getTrendingMovies();
       setFilmes(listaDeFilmes);
     };
-
     carregarDados();
   }, []);
 
   return (
-  <>
-  <Header />
-    <div>
-      <ul>
-        {filmes.map((filme) => (
-          <li key={filme.id}><img src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`} alt={filme.title} /></li>
+    <div className="slider-container">
+      <Swiper
+        modules={[Navigation, Virtual]}
+        navigation={true}
+        spaceBetween={20}
+        slidesPerView={7.5}
+        allowTouchMove={true}
+        loop={true}
+        breakpoints={{
+          320: { slidesPerView: 4.5 },
+          1024: { slidesPerView: 5.5 },
+        }}
+      >
+        {/* O .map deve envolver o SwiperSlide! */}
+        {filmes.map((filme, index) => (
+          <SwiperSlide key={filme.id} virtualIndex={index}>
+            <div className="movie-card">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${filme.poster_path}`}
+                alt={filme.title}
+                className="movie-poster"
+              />
+            </div>
+          </SwiperSlide>
         ))}
-      </ul>
+      </Swiper>
     </div>
-  </>
   );
-}
+};
 export default Home;
